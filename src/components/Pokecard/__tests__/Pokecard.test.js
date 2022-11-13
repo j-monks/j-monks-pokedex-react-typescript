0f3bbf3eaab8bff6132f20686ac0df3127250cec
James Monks <james.m@pinnaclevl.com>
2022-11-13 17:07:56 +0000

    Added a click event test for pokecard

--- src/components/Pokecard/__tests__/Pokecard.test.js
+++ src/components/Pokecard/__tests__/Pokecard.test.js
@@ -1,4 +1,5 @@
 import { render, screen, cleanup } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import renderer from 'react-test-renderer'
 import '@testing-library/jest-dom'
 import Pokecard from '../Pokecard'
@@ -25,4 +26,17 @@ test('matches snapshot', () => {
     }
     const tree = renderer.create(<Pokecard name={pokecard.name} spriteUrl={pokecard.spriteUrl}/>).toJSON()
     expect(tree).toMatchSnapshot()
+})
+
+test('should call the onClick when pokecard is clicked', () => {
+    const onClickPokemon = jest.fn()
+    const pokecard = {
+        spriteUrl: 'http://img.pokemondb.net/sprites/black-white/normal/bulbasaur.png',
+        name: 'bulbasaur'
+
+    }
+    render(<Pokecard name={pokecard.name} onClickPokemon={onClickPokemon} spriteUrl={pokecard.spriteUrl}/>)
+    const pokecardElement = screen.getByTestId('pokecard-bulbasaur')
+    userEvent.click(pokecardElement)
+    expect(onClickPokemon).toBeCalled()
 })
